480be3fb73ebaa36c9358bf2000199e5b8295d0e
glnsb <marcusorhan@gmail.com>
2022-12-30 01:59:43 -0500

    Glnsb tf-2 start

---
+++ 01main.js
@@ -0,0 +1,9 @@
+
+
+function myFunction() {
+document.getElementById("demo").innerHTML = "Become a PRO Team Fortres2 GAMER ";
+}
+
+
+// alert(' You are about to learn every Strategy, trick, move, shortcut to be a pro Teamfortress2 player! Are you ready to start your exciting TF2 journey?')
+// error('Suspicious malvare detected')
