b350ba0587b95b0b5d72ee8d9fe2ba8d59dd2bcd
glnsb <marcusorhan@gmail.com>
2022-12-31 23:15:45 -0500

    some changes in css

--- 01main.js
+++ 01main.js
@@ -1,9 +1,9 @@
 
 
 function myFunction() {
-document.getElementById("demo").innerHTML = "Become a PRO Team Fortres2 GAMER ";
+document.getElementById("demo").innerHTML = "To Become a PRO Team Fortress2 GAMER the very first trick is: 1. Respect the snipers sigthline! to avoid getting killed by aq sniper. Make your moves unpredictable by other players. Move right, left, Jump and couch.";
 }
 
 
-// alert(' You are about to learn every Strategy, trick, move, shortcut to be a pro Teamfortress2 player! Are you ready to start your exciting TF2 journey?')
+// alert(' You are about to learn every Strategy, trick, move, shortcut to be a pro Teamfortress2 player! Are you ready to start your exciting TF2 journey? click ok')
 // error('Suspicious malvare detected')
