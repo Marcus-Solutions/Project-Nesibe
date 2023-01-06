cca159a0f9d8ff17eec14aef0757ddadcc9d531f
glnsb <marcusorhan@gmail.com>
2023-01-06 01:00:46 -0500

    css changes in the form

--- 01main.js
+++ 01main.js
@@ -1,7 +1,7 @@
 
 
 function myFunction() {
-document.getElementById("demo").innerHTML = "To Become a PRO Team Fortress2 GAMER the very first trick is: 1. Respect the snipers sigthline! to avoid getting killed by aq sniper. Make your moves unpredictable by other players. Move right, left, Jump and couch.";
+document.getElementById("demo").innerHTML = "Trick1. Respect the snipers sigthline! ";
 }
 
 
@@ -9,3 +9,4 @@ document.getElementById("demo").innerHTML = "To Become a PRO Team Fortress2 GAME
 
 // alert(' You are about to learn every Strategy, trick, move, shortcut to be a pro Teamfortress2 player! Are you ready to start your exciting TF2 journey? click ok')
 // error('Suspicious malvare detected')
+//"to avoid getting killed by aq sniper. Make your moves unpredictable by other players. Move right, left, Jump and couch."
