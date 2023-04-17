fc1c7e80a5a8edec65a571c9ef87176b9d596121
glnsb <marcusorhan@gmail.com>
2023-04-16 23:55:21 -0400

    minor css changes

--- 01main.js
+++ 01main.js
@@ -1,12 +1,12 @@
 
 
 function myFunction() {
-document.getElementById("demo").innerHTML = "Trick1. Respect the snipers sigthline! ";
+document.getElementById("contact").innerHTML = "class=login-box";
 }
 
 
 
 
-// alert(' You are about to learn every Strategy, trick, move, shortcut to be a pro Teamfortress2 player! Are you ready to start your exciting TF2 journey? click ok')
-// error('Suspicious malvare detected')
-//"to avoid getting killed by aq sniper. Make your moves unpredictable by other players. Move right, left, Jump and couch."
+ alert(' You are about to learn every Strategy, trick, move, shortcut to be a pro Teamfortress2 player! Are you ready to start your exciting TF2 journey? click ok')
+ error('Suspicious malvare detected')
+"to avoid getting killed by aq sniper. Make your moves unpredictable by other players. Move right, left, Jump and couch."
