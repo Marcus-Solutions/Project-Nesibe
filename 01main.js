097100bbaeb134ce5a6bc62529962f91c4948bb8
glnsb <marcusorhan@gmail.com>
2023-06-24 13:32:13 -0400

    02Confirm.html added

--- 01main.js
+++ 01main.js
@@ -4,9 +4,6 @@ function myFunction() {
 document.getElementById("contact").innerHTML = "class=login-box";
 }
 
-
-
-
  alert('En guzel fotograflarda bulusmak icin, click ok')
  error('Anilariniz ucup gitti mi? Sevdiklerin simdi ulasilmaz uzaklarda m?')
 "Caresi, unutulmaz anilari unutulmaz pozlarda saklamak. ulasin birlikte kaydedelim."
